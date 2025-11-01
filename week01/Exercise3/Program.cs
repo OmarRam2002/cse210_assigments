643a5a63d880034fae10f8525d21f76ccbd556e4
OmarRam2002 <omarramirezasencio@gmail.com>
2025-11-01 13:16:29 -0600

    completed the assigment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        int randomNumber = new Random().Next(1, 101);
+        int userGuessInput;
+        int guessCount = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuessInput = int.Parse(Console.ReadLine());
+            guessCount++;
+            if (randomNumber == userGuessInput)
+            {
+                Console.WriteLine("You guessed it.");
+            }
+            else if (userGuessInput < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (randomNumber != userGuessInput);
+
+        Console.WriteLine($"It took you {guessCount} guesses.");
     }
 }
