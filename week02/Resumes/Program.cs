f204e52c41b2577462dda0ab54223a093bd82d3e
OmarRam2002 <omarramirezasencio@gmail.com>
2025-11-03 21:53:15 -0700

    started

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Corp";
+
+        job1.DisplayMethod();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Data Inc.";
+        job2.DisplayMethod();
+
     }
 }
